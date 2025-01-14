861c7380ad23eced2cc2a2e30f55202aab8daa98
M0hamady <119875626+M0hamady@users.noreply.github.com>
2025-01-15 00:36:44 +0200

    update in notification

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import ForgotPassword from "./components/Auth/ForgotPassword";
 import ResetPassword from "./components/Auth/ResetPassword";
@@ -15,34 +14,35 @@ import ProductDetail from "./pages/ProductDetail/ProductDetail";
 import Footer from "./components/Footer";
 import { farmBackground, farmLogo } from "./assets";
 import Profile from "./components/Auth/Profile";
+import NotificationList from "./components/NotificationList";
+import { NotificationProvider } from "./context/NotificationContext";
+import Login from "./components/Auth/Login";
 
 const App: React.FC = () => {
   return (
-    <Router> {/* وضع Router في الأعلى */}
-      <AuthProvider> {/* AuthProvider داخل Router */}
-        <LoadingScreen images={[farmLogo, farmBackground]}>
-          <Header />
-          <Routes>
-            {/* Authentication Routes */}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/reset-password" element={<ResetPassword />} />
-
-            {/* App Routes */}
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/products" element={<Products />} />
-            <Route path="/products/:id" element={<ProductDetail />} />
-            <Route path="/profile" element={<Profile />} />
-
-            {/* Default to Login */}
-            <Route path="*" element={<Login />} />
-          </Routes>
-          <Footer />
-        </LoadingScreen>
+    <Router>
+        <NotificationProvider> {/* Wrap with NotificationProvider */}
+      <AuthProvider>
+          <LoadingScreen images={[farmLogo, farmBackground]}>
+            <Header />
+            <NotificationList /> {/* Add NotificationList to show notifications */}
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/reset-password" element={<ResetPassword />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/products/:id" element={<ProductDetail />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="*" element={<Login />} />
+            </Routes>
+            <Footer />
+          </LoadingScreen>
       </AuthProvider>
+        </NotificationProvider>
     </Router>
   );
 };
